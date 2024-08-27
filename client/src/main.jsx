d6cf57f7f6253804616039e8e073ee67f51ca800
Zfastroot <zakaziko1305@gmail.com>
2024-08-28 00:49:46 +0100

    creation of the routing for the pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
-import App from './App.jsx'
 import './index.css'
+import HomePage from "./routes/homepage/HomePage"
+import DashboardLayout from './layouts/dashboardlayout/DashboardLayout';
+import RootLayout from './layouts/rootlayout/RootLayout';
+import SignInPage from './routes/signinpage/SignInPgae'
+import SignUpPage from './routes/signuppage/SignUpPage'
+import ChatPage from './routes/chatpage/ChatPage'
+import DashboardPage from './routes/dashboardpage/DashboardPage'
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/sign-in/*",
+        element: <SignInPage />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUpPage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
