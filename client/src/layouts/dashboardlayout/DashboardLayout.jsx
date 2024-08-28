adac88bea44e2a147cda82f7512dbc0c1ca0ca12
Zfastroot <zakaziko1305@gmail.com>
2024-08-28 02:47:06 +0100

    config the Dashboard layout authentification with clerk functions

--- client/src/layouts/dashboardlayout/DashboardLayout.jsx
+++ client/src/layouts/dashboardlayout/DashboardLayout.jsx
@@ -1,10 +1,22 @@
-import { Outlet,  } from "react-router-dom";
+import { Outlet,useNavigate  } from "react-router-dom";
 import "./dashboardlayout.css";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
 
 
 const DashboardLayout = () => {
+  const { userId, isLoaded } = useAuth();
 
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (isLoaded && !userId) {
+      navigate("/sign-in");
+    }
+  }, [isLoaded, userId, navigate]);
+
+  if (!isLoaded) return "Loading...";
 
   return (
     <div className="dashboardlayout">
