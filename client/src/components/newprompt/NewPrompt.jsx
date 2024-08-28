b02609d682294ce1527f074e164e586e16f990b6
hajarloraoui <hajar.loraoui12@gmail.com>
2024-08-28 10:39:33 +0000

    add css of newprompt

--- client/src/components/newprompt/NewPrompt.jsx
+++ client/src/components/newprompt/NewPrompt.jsx
@@ -18,11 +18,11 @@ function NewPrompt() {
             <label htmlFor="file">
                 <img src="/attachment.png" alt="" />
             </label>
-        <input id="file" type="file" multiple={false} hidden />
-        <input type="text" name="text" placeholder="Ask anything..." />
-        <button>
-            <img src="/arrow.png" alt="" />
-        </button>
+            <input id="file" type="file" multiple={false} hidden />
+            <input type="text" name="text" placeholder="Ask anything..." />
+            <button>
+                <img src="/arrow.png" alt="" />
+            </button>
         </form>
     </>
   );
