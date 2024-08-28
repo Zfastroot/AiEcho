34ac149ecc3e1c2819c05180a066632c1f01a0e2
Zfastroot <zakaziko1305@gmail.com>
2024-08-28 18:41:51 +0100

    generate all the backen we will need + add some edit in some pages

--- client/src/routes/dashboardpage/DashboardPage.jsx
+++ client/src/routes/dashboardpage/DashboardPage.jsx
@@ -1,17 +1,43 @@
-import "./dashboardpage.css"
-import React from 'react'
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import "./dashboardpage.css";
+import { useNavigate } from "react-router-dom";
 
+const DashboardPage = () => {
+  const queryClient = useQueryClient();
 
-function DashboardPage() {
+  const navigate = useNavigate();
 
-  
+  const mutation = useMutation({
+    mutationFn: (text) => {
+      return fetch(`${import.meta.env.VITE_API_URL}/api/chats`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      }).then((res) => res.json());
+    },
+    onSuccess: (id) => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["userChats"] });
+      navigate(`/dashboard/chats/${id}`);
+    },
+  });
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+
+    mutation.mutate(text);
+  };
   return (
     <div className="dashboardpage">
       <div className="texts">
         <div className="logo">
           <img src="/logo.png" alt="" />
-          <h1>ECHO AI</h1>
+          <h1>LAMA AI</h1>
         </div>
         <div className="options">
           <div className="option">
@@ -29,7 +55,7 @@ function DashboardPage() {
         </div>
       </div>
       <div className="formContainer">
-        <form >
+        <form onSubmit={handleSubmit}>
           <input type="text" name="text" placeholder="Ask me anything..." />
           <button>
             <img src="/arrow.png" alt="" />
@@ -37,8 +63,7 @@ function DashboardPage() {
         </form>
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default DashboardPage
+export default DashboardPage;
