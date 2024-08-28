de8d32b673560087707bc12d1264c6c0407c2c00
Zfastroot <zakaziko1305@gmail.com>
2024-08-28 02:37:31 +0100

    add lerck provider in the root layout hj you forget to add it

--- client/src/layouts/rootlayout/RootLayout.jsx
+++ client/src/layouts/rootlayout/RootLayout.jsx
@@ -1,22 +1,35 @@
 import { Link, Outlet } from "react-router-dom";
 import "./rootlayout.css";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
 import React from "react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 function RootLayout() {
   return (
-    <div className="rootlayout">
-      <header>
-        <Link to="/" className="logo">
-          <img src="/logo.png" alt="" />
-          <span>ECHO AI</span>
-        </Link>
-        <div className="user">
-        </div>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </div>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <div className="rootlayout">
+        <header>
+          <Link to="/" className="logo">
+            <img src="/logo.png" alt="" />
+            <span>ECHO AI</span>
+          </Link>
+          <div className="user">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </ClerkProvider>
+
   );
 }
 
